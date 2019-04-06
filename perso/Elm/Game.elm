9bab9d2a7dd0a24670892ce9c58fad0a15db7374
Lucas Schürer <39245000+LucasSchurer@users.noreply.github.com>
2019-04-06 18:18:17 -0300

    Update Game.elm
    Montando o esqueleto do programa

--- perso/Elm/Game.elm
+++ perso/Elm/Game.elm
@@ -1,22 +1,56 @@
 import Browser
-import Html exposing (..)
-import Html.Attributes
-import Html.Browser
-import Svg exposing (..)
-import Svg.Attributes exposing (..)
+import Html exposing (div)
 
+-- Main
 main = 
     Browser.element
-      { init = init
-      , view = view
-      , update = update
-      , subscriptions = subscriptions
-      }
-    
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
 -- Model
+
+type alias Player =    
+    Int
+
+init : () -> ( Player, Cmd Msg )
+init _ =
+    ( 0
+    , Cmd.none 
+    )
 
 -- Update
 
+type Direction 
+    = Left
+    | Up
+    | Right
+    | Down
+    | None
+
+type Msg 
+    = KeyPressed Direction
+
+update : Msg -> Player -> ( Player, Cmd Msg )
+update msg player = 
+    case msg of
+        KeyPressed _ ->
+            ( player
+            , Cmd.none
+            )
+
 -- Subscriptions
 
+subscriptions : Player -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
 -- View
+
+view : Player -> Html.Html Msg
+view _ = 
+    div [] []
+    
