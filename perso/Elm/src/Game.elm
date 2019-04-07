f6ad539cf8286d34f1de82a9a64d98ec5c793590
Lucas Schürer <39245000+LucasSchurer@users.noreply.github.com>
2019-04-06 23:57:42 -0300

    Update Game.elm
    - Generalização do atributo de tamanho da tela, para ficar mais fácil de redimensionar todos os objetos do jogo dependendo do tamanho da tela.
    
    - Definição dos valores para cada um dos players, desenhando os dois na tela, mas ainda sem movimentação.

--- perso/Elm/src/Game.elm
+++ perso/Elm/src/Game.elm
@@ -19,8 +19,16 @@ type State
     = Play
     | Pause
 
-type alias GameSettings =
+type alias WindowSettings =
     { width : Int
+    , height : Int
+    , focus : Bool
+    }
+
+type alias GameSettings =
+    { x : Int
+    , y : Int
+    , width : Int
     , height : Int
     , state : State
     }
@@ -37,47 +45,60 @@ type alias Model =
     { player1 : Player
     , player2 : Player
     , game : GameSettings
+    , window : WindowSettings
     }
 
 init : () -> ( Model, Cmd Msg )
 init _ =
     let
-        width = 600
-        height = 400
+        width = 1920
+        height = 1080
 
     in
     ( Model 
         ( initPlayer 
-            ( width//2 + 10 ) 
-            ( height//2 + 10 ) 
+            ( width // 2 - width // 4 + 30 ) 
+            ( 50 ) 
         ) 
         ( initPlayer 
-            ( width//2 + 10 ) 
-            ( height//2 + 10 ) 
+            ( width // 2 + width // 4 - 60 ) 
+            ( 50 ) 
         )
         ( initGameSettings width height )
+        ( initWindowSettings width height )
     , Cmd.none 
     )
 
 initPlayer : Int -> Int -> Player
 initPlayer x y = 
-    Player x y 0 0 0
+    Player x y 30 60 0
 
 initGameSettings : Int -> Int -> GameSettings
 initGameSettings width height =
-    GameSettings width height Play
+    GameSettings 
+        ( width // 2 - width // 4 ) 
+        0 
+        ( width // 2 ) 
+        ( height // 2 ) 
+        Play
+
+initWindowSettings : Int -> Int -> WindowSettings
+initWindowSettings width height =
+    WindowSettings width height True
 
 -- Update
 
-type Direction 
+type Key 
     = Left
     | Up
     | Right
     | Down
+    | Space
     | None
 
 type Msg 
-    = KeyPressed Direction
+    = KeyPressed Key
+    | GameState State
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model = 
@@ -105,6 +126,8 @@ toDirection string =
         "ArrowDown" ->
             KeyPressed Down
 
+        "Space" -> 
+            KeyPressed Space
         _ -> 
             KeyPressed None
 
@@ -119,20 +142,41 @@ subscriptions _ =
 view : Model -> Svg.Svg Msg
 view model = 
     Svg.svg 
-        [ Svg.Attributes.width "800"
-        , Svg.Attributes.height "800"
-        , Svg.Attributes.viewBox "0 0 800 800"
+        [ Svg.Attributes.width ( String.fromInt ( model.window.width - model.window.width // 10) )
+        , Svg.Attributes.height ( String.fromInt ( model.window.height - model.window.height // 7 ) )
+        , Svg.Attributes.viewBox (
+            String.join " " 
+                [ "0"
+                , "0"
+                , ( String.fromInt ( model.window.width - model.window.width // 10 ) )
+                , ( String.fromInt ( model.window.height - model.window.height // 7 ) )
+                ]
+        )
         ]
-        [ drawBackground
+        [ drawBackground model.game
+        , drawPlayer model.player1
+        , drawPlayer model.player2
         ]    
         
-drawBackground : Svg.Svg Msg
-drawBackground = 
+drawBackground : GameSettings -> Svg.Svg Msg
+drawBackground settings = 
     Svg.rect
-        [ Svg.Attributes.x "10" 
-        , Svg.Attributes.y "10"
+        [ Svg.Attributes.x ( String.fromInt settings.x ) 
+        , Svg.Attributes.y ( String.fromInt settings.y )
         , Svg.Attributes.rx "10"
         , Svg.Attributes.ry "10"
-        , Svg.Attributes.width "400"
-        , Svg.Attributes.height "600"
+        , Svg.Attributes.width ( String.fromInt settings.width )
+        , Svg.Attributes.height ( String.fromInt settings.height )
+        ] []
+
+drawPlayer : Player -> Svg.Svg Msg
+drawPlayer player =
+    Svg.rect
+        [ Svg.Attributes.x ( String.fromInt player.x ) 
+        , Svg.Attributes.y ( String.fromInt player.y )
+        , Svg.Attributes.rx "10"
+        , Svg.Attributes.ry "10"
+        , Svg.Attributes.width ( String.fromInt player.width )
+        , Svg.Attributes.height ( String.fromInt player.height )
+        , Svg.Attributes.fill "white"
         ] []
