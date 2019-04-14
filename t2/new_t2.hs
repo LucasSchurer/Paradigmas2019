c6517d3dfb300ffb9bfff6e03f20bb179b097eaf
Lucas <lschurer@inf.ufsm.br>
2019-04-13 21:48:37 -0300

    Update t2

--- t2/new_t2.hs
+++ t2/new_t2.hs
@@ -20,6 +20,17 @@ degreesToRad :: Float -> Float
 degreesToRad degrees =
     degrees * ( pi / 180 )
 
+
+-- Funções para gerar cores.
+
+getColour :: Int -> Colour
+getColour x
+    | x == 1 = ( 255, 0, 0 ) -- Vermelho
+    | x == 2 = ( 0, 0, 255 ) -- Verde
+    | x == 3 = ( 0, 0, 255 ) -- Azul
+    | otherwise = ( 0, 0, 0 ) -- Preto
+
+
 -- Geração das formas geométricas utilizadas em cada caso.
 
 genRects :: Int -> Int -> [Rect]
@@ -37,3 +48,32 @@ genRects c l =
         ( width, height ) = ( 50, 50 )
         gap = 10
         ( r, g, b ) = ( 100, 100, 100 )
+
+
+-- Geração das strings contendo as figuras em SVG.
+
+svgRects :: Rect -> String
+svgRects ( ( x, y ), w, h, ( r, g, b) ) =
+    printf "<rect x = '%.2f' y = '%.2f' width = '%.2f' height = '%.2f' style = 'fill:rgb(%.2f, %.2f, %.2f)'/>" x y w h r g b
+
+
+-- Geração das strings contendo elementos básicos em SVG.
+
+svgBegin :: Float -> Float -> String
+svgBegin w h =
+    printf "<svg width='%.2f' height='%.2f' xmlns='http://www.w3.org/2000/svg'>\n" w h
+
+svgEnd :: String
+svgEnd =
+    "</svg>"
+
+
+-- Casos
+
+case1 :: IO ()
+case1 = do
+    writeFile "case1.svg" $ svgstrs
+    where
+        svgstrs = svgBegin w h ++ svgrects ++ svgEnd
+        svgrects = concat $ map svgRects $ genRects 10 5
+        ( w, h ) = ( 1920, 1080 )
